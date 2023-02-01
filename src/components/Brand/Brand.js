8b5c07ebe504cfc32c60d6643d12f5f6506d437f
orun-sunny~ <sunny.mukut@gmail.com>
2023-02-01 13:14:05 +0600

    Crud-done

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {useForm} from "react-hook-form";
 
-const adminToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc2Uuc2Vsb3BpYW4udXNcL2FwaVwvYWRtaW5cL2xvZ2luIiwiaWF0IjoxNjc1MTYxNTgwLCJleHAiOjE2NzUxNjUxODAsIm5iZiI6MTY3NTE2MTU4MCwianRpIjoiOXlsemZVb2tlYVJhcXpTcCIsInN1YiI6MSwicHJ2IjoiZGY4ODNkYjk3YmQwNWVmOGZmODUwODJkNjg2YzQ1ZTgzMmU1OTNhOSJ9.xPYKbVWUBbq9G07FjGiBJsy6jC5QCLb7LUdjHwx9s18";
+const adminToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc2Uuc2Vsb3BpYW4udXNcL2FwaVwvYWRtaW5cL3JlZnJlc2giLCJpYXQiOjE2NzUyMjc0OTUsImV4cCI6MTY3NTIzNjA1MywibmJmIjoxNjc1MjMyNDUzLCJqdGkiOiIyeGV4Y2w0YlpjeHEwd2RQIiwic3ViIjoxLCJwcnYiOiJkZjg4M2RiOTdiZDA1ZWY4ZmY4NTA4MmQ2ODZjNDVlODMyZTU5M2E5In0._L7qLB76Q3n4UpJkOS-_S9dcKdMB7tVjNm7WnXErlgc";
 
 const Brand = () => {
     const [brands, setBrands] = useState([])
     const [categories, setCategories] = useState([])
+    const [catId,setCatId] = useState(null)
     const [subCategories, setSubcategories] = useState([])
     const [img, setImgUpload] = useState();
     // useEffect(() => {
@@ -23,23 +24,20 @@ const Brand = () => {
 
     const onSubmit = async (data) => {
         console.log(data)
-        // const formData =new FormData();
-        // formData.append("image",data.image[0]);
-        // formData.append("name",data.name);
-        // for(let pair of formData.entries()){
-        //     console.log(pair[0]," ",pair[1])
-        // }
-        // const res = await fetch('https://se.selopian.us/api/admin/brand-store', {
-        //     method: 'POST',
-        //     body: formData,
-        //     headers: {
-        //         // 'content-type': 'application/json',
-        //         'Authorization':`bearer ${adminToken}`
-        //     }
-        // })
-        // console.log(res)
-        // // const newData = await res.json()
-        // // console.log(newData)
+        const formData =new FormData();
+        formData.append("image",data.image[0]);
+        formData.append("name",data.name);
+        formData.append("sub_category_id",data.sub_category_id);
+        formData.append("category_id",catId);
+        const res = await fetch('https://se.selopian.us/api/admin/brand-store', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Authorization':`bearer ${adminToken}`
+            }
+        })
+        const newData = await res.json()
+        console.log(newData)
     };
 
     const getCategories = async () => {
@@ -56,7 +54,7 @@ const Brand = () => {
     }
 
     const getSubcategories = async () => {
-        const res = await fetch("https://se.selopian.us//api/admin/sub-category-list", {
+        const res = await fetch("https://se.selopian.us/api/admin/sub-category-list", {
             method: "GET",
             headers: {
                 'Authorization': `bearer ${adminToken}`
@@ -64,43 +62,40 @@ const Brand = () => {
         })
         if (res.status === 200) {
             const data = await res.json()
+            // console.log(data)
             setSubcategories(data.subCategories)
         }
     }
 
-    console.log(categories)
-    console.log(subCategories)
+    // console.log(categories)
 
     useEffect(() => {
         getCategories()
-    }, [])
-
-    useEffect(() => {
         getSubcategories()
     }, [])
 
+const selectedSubCats = subCategories.filter(subs => +(subs.category_id) === +catId)
 
     return (
         <div>
             <h1>Brand: {brands.length}</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("name")} type="text" placeholder='Name' required/>
-                <input {...register("image")} type="file" placeholder='image' accept="image/*" required/>
-                <input type="submit" value="Add Brand"/>
-                <select {...register("category_id")} defaultValue={0}>
+                {/*<select {...register("category_id")} defaultValue={0}>*/}
+                <select onClick={(e)=>setCatId(e.target.value)} defaultValue={0}>
                     {
                         categories.map(item => <option key={item.id} value={item.id}>{item.name}</option>)
                     }
-
-                </select>
-
+                </select><br/>
                 <select {...register("sub_category_id")} defaultValue={0}>
                     {
-                        categories.map(item => <option key={item.id} value={item.id}>{item.name}</option>)
+                        selectedSubCats.map(item => <option key={item.id} value={item.id}>{item.name} </option>)
                     }
-
-                </select>
+                </select><br/>
+                <input {...register("name")} type="text" placeholder='Name' required/><br/>
+                <input {...register("image")} type="file" placeholder='image' accept="image/*" required/><br/>
+                <input type="submit" value="Add Brand"/><br/>
             </form>
+
 
             {/*<ul>*/}
             {/*    {*/}
